ee5e1a400c7c26738fbff71fbb64d91165685c13
alamincoders <developeralamin03@gmail.com>
2022-10-21 21:47:46 +0600

    react router setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import { store } from "./app/store";
 import "./index.css";
@@ -11,7 +12,11 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
